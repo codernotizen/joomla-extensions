4ce7d64a79608aeb0555a7583eac01a1ab565835
codernotizen <developer@dertechnokrat.de>
2013-06-10 14:18:10 +0200

    added database tables for jukebox joomla-2.5 component

---
+++ joomla-2.5/jukebox-component/admin/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `#_jukebox_artists`;
+DROP TABLE IF EXISTS `#_jukebox_songs`;
+DROP TABLE IF EXISTS `#_jukebox_genre`;
+DROP TABLE IF EXISTS `#_jukebox_hosts`;
+DROP TABLE IF EXISTS `#_jukebox_performance`;
+DROP TABLE IF EXISTS `#_jukebox_solo_artists`;
+DROP TABLE IF EXISTS `#_jukebox_bands`;
+DROP TABLE IF EXISTS `#_jukebox_cooperations`;
+
+
+
